025cbc2c1f574c8ecb7731e4864feef584085f08
Evgeniy0y0 <sievati.yevhen@student.sumdu.edu.ua>
2026-04-15 13:38:23 +0300

    Updated validate.js

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,17 +1,11 @@
 const { validationResult } = require('express-validator');
+const ApiError = require('../errors/ApiError');
 
 module.exports = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({
-      success: false,
-      message: 'Validation error',
-      errors: errors.array().map(e => ({
-        field: e.path,
-        msg: e.msg
-      })),
-      statusCode: 400
-    });
+    const errorList = errors.array().map(e => ({ field: e.path, msg: e.msg }));
+    throw ApiError.badRequest('Validation error', errorList);
   }
   next();
 };
